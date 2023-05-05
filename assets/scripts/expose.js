c9a858f1413d0ff6c4a44f35dedd2b7236ca8e4b
Gabe Salvania <bsalvania@ucsed.edu>
2023-05-05 16:08:31 -0700

    Added audio for horns

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,16 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const parentDOM = document.getElementById("expose").value;
+  const parentDOM2 = document.getElementById("horn-select");
+  let button = document.querySelector('button');
+
+  button.addEventListener("click", playSound);
+}
+
+
+function playSound() {
+  // Picks audio track by finding the value of horn
+  let sound = new Audio("assets/audio/" + document.getElementById("horn-select").value + ".mp3");
+  sound.play();
 }

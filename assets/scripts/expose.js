9c70f050f541e7e90dc16f2f09befbe165fb08fd
Gabe Salvania <bsalvania@ucsed.edu>
2023-05-05 16:45:17 -0700

    Image changes

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,16 +4,29 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  const parentDOM = document.getElementById("expose").value;
-  const parentDOM2 = document.getElementById("horn-select");
-  let button = document.querySelector('button');
 
-  button.addEventListener("click", playSound);
+  //const image = document.querySelector('img[alt="No image selected"]');
+  const image = document.querySelector('img');
+  const sound = document.querySelector('audio');
+
+  const horn = document.getElementById("horn-select");
+  horn.addEventListener("change", (event) => {
+    sound.src = "assets/audio/" + document.getElementById("horn-select").value + ".mp3";
+    image.src = "assets/images/" + document.getElementById("horn-select").value + ".svg";
+  });
+
+  const button = document.querySelector('button');
+  button.addEventListener("click", function() {
+    sound.play();
+  });
+
+
+  
 }
 
 
-function playSound() {
-  // Picks audio track by finding the value of horn
-  let sound = new Audio("assets/audio/" + document.getElementById("horn-select").value + ".mp3");
-  sound.play();
-}
+// function playSound() {
+//   // Picks audio track by finding the value of horn
+//   let sound = new Audio("assets/audio/" + document.getElementById("horn-select").value + ".mp3");
+//   sound.play();
+// }

fe18eb04a194d61ff90afda8aad4df7ab17428bd
Gabe Salvania <bsalvania@ucsed.edu>
2023-05-05 17:05:19 -0700

    Added confetti

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,32 +1,30 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
+const jsConfetti = new JSConfetti();
 
 function init() {
   // TODO
 
-  //const image = document.querySelector('img[alt="No image selected"]');
-  const image = document.querySelector('img');
+  const image = document.querySelector('img[src="assets/images/no-image.png"]');
   const sound = document.querySelector('audio');
 
   const horn = document.getElementById("horn-select");
   horn.addEventListener("change", (event) => {
-    sound.src = "assets/audio/" + document.getElementById("horn-select").value + ".mp3";
-    image.src = "assets/images/" + document.getElementById("horn-select").value + ".svg";
+    sound.src = `assets/audio/${horn.value}.mp3`;
+    image.src = `assets/images/${horn.value}.svg`;
   });
 
   const button = document.querySelector('button');
   button.addEventListener("click", function() {
     sound.play();
+    if (horn.value == 'party-horn'){
+      jsConfetti.addConfetti();
+    }
   });
+
+
 
 
   
 }
-
-
-// function playSound() {
-//   // Picks audio track by finding the value of horn
-//   let sound = new Audio("assets/audio/" + document.getElementById("horn-select").value + ".mp3");
-//   sound.play();
-// }

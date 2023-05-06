f2ec71999200ceeef4d31407a81c619096abf139
Gabe Salvania <bsalvania@ucsed.edu>
2023-05-05 18:19:12 -0700

    Finished speech synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,19 +3,59 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
-  const image = document.querySelector('img[src="assets/images/smiling.png"]');
-  while (SpeechSynthesis.speaking) {
-    image.src = "smiling-open.png";
+  const synth = window.speechSynthesis;
+
+  const inputForm = document.querySelector("form");
+  const inputTxt = document.querySelector(".txt");
+  const voiceSelect = document.querySelector("select");
+  const pitch = document.querySelector("#pitch");
+  const pitchValue = document.querySelector(".pitch-value");
+  const rate = document.querySelector("#rate");
+  const rateValue = document.querySelector(".rate-value");
+  
+  let voices = [];
+  
+  function populateVoiceList() {
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
   }
 
-  let speech = new SpeechSynthesisUtterance();
+  const image = document.querySelector('img[src="assets/images/smiling.png"]');
+
+
+  //let speech = new SpeechSynthesisUtterance();
   const text = document.querySelector("textarea");
   const button = document.querySelector('button');
   button.addEventListener("click", function() {
-    speech.speak(text.value);
-  //   let utterance = new SpeechSynthesisUtterance("Hello world!");
-  // speechSynthesis.speak(utterance);
+    const option = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    const speech = new SpeechSynthesisUtterance(text.value);
+    for(let i = 0; i < voices.length; i++)
+    {
+      if(voices[i].name == option)
+      speech.voice = voices[i];
+    }
+    synth.speak(speech);
+    image.src = "assets/images/smiling-open.png";
+    speech.addEventListener("end", function() {
+      image.src = "assets/images/smiling.png";
+    });
   });
 
 }

7208e83fdca111ac134f42664f2391a69fa7c915
Gabe Salvania <bsalvania@ucsed.edu>
2023-05-05 17:44:05 -0700

    Testing part 2

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,18 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const image = document.querySelector('img[src="assets/images/smiling.png"]');
+  while (SpeechSynthesis.speaking) {
+    image.src = "smiling-open.png";
+  }
+
+  let speech = new SpeechSynthesisUtterance();
+  const text = document.querySelector("textarea");
+  const button = document.querySelector('button');
+  button.addEventListener("click", function() {
+    speech.speak(text.value);
+  //   let utterance = new SpeechSynthesisUtterance("Hello world!");
+  // speechSynthesis.speak(utterance);
+  });
+
 }
